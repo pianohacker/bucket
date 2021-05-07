b6437e727ca127530d77b4337c7f25c439576a35
Jesse Weaver <pianohacker@gmail.com>
2021-05-06 23:46:33 -0600

    Add MPL-2.0 license header

--- board.lua
+++ board.lua
@@ -1,3 +1,9 @@
+-- Copyright (c) Jesse Weaver, 2021
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 -- Board logic and drawing
 --
 -- The Bucket board uses a radial coordinate system, (t, r), where t is between 1 and self.circumf,
--- test/common_tests.lua
+++ test/common_tests.lua
@@ -1,3 +1,9 @@
+-- Copyright (c) Jesse Weaver, 2021
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 local common = require("test.common")
 
 notion("dedent works correctly", function()
